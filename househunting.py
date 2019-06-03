51cd247bba966ceb198e949698516a87fca360cd
christopherwburke <christopher.w.burke@gmail.com>
2019-06-03 18:48:38 -0400

    It's not working but I am so frustrated I have to take a break.

--- househunting.py
+++ househunting.py
@@ -1,39 +1,29 @@
-# househunting assignment
-
-#solve for how many months needed to save for down payment
-#ask input for annual_salary , portion_saved, and total_cost
-
-#Months = Down Payment Amount / portion_saved
-#Down Payment Amount = portion_down_payment * total_cost
-
 portion_down_payment = 0.25
 
 current_savings = 0
 
-r = .04
+r = (.04/12)
 
-annual_salary = input("What is your annual salary? ")
+#ask input for annual_salary , savings_rate, and cost_of_home
 
-portion_saved = input("What percentage of your annual salary will you save each month? Answer in the form of a decimal. ")
+annual_salary=int(
+    input("What is your annual salary? "))
 
-total_cost = input("How expensive is your dream home? ")
+savings_rate=float(input("What percentage of your annual salary will you save each month? Answer in the form of a decimal. "))
 
-Months = portion_down_payment * total_cost / portion_saved
+cost_of_home=int(input("How expensive is your dream home? "))
 
-print(Months)
+#Down Payment Amount = portion_down_payment * total_cost
 
-# current savings at i = 0, 0
-# current savings at i = n, (i= n-1) + (annual_salary * portion_saved / 12) + (current_savings * r/12)
+down_payment = portion_down_payment * cost_of_home
 
-# at end of month, 
-# i = 0
-# while i 
+### monthly_savings = annual salary /12 * savings rate ###
 
-# test for money saved
-# keep_going = True
-# while keep_going:
-# while 
-# if current_savings = (total_cost * portion_down_payment):
-    #print(Months)
-# else 
+monthly_savings = (annual_salary / 12) * savings_rate
 
+months = 0
+
+while (current_savings < down_payment):
+    current_savings = current_savings + ((current_savings + monthly_savings)*r)
+    months=months+1
+    print(months)
